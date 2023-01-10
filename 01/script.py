369f5e7b7b40e6d972d9a435b628ab75ba829c0a
alancooper509 <alancooper509@hotmail.com>
2023-01-10 15:16:49 -0500

    part two - electric boogaloo

--- 01/script.py
+++ 01/script.py
@@ -1,43 +1,41 @@
 import sys
 
-class ElfItems:
-    items = []
-    sum = None
-    def __init__(self, items):
-        """
-        Gets various statistics based off of input item values
-        """
-        self.items = items
-        self.sum = sum(items)
-
-def main(fname):
-    # read from file 
+def main(fname, maxCount=1):
+    if (not isinstance(fname, str)):
+        return
+    if (not maxCount.isdigit()):
+        return
+    # read from file
     input = None
     with open(fname, "r+") as file:
         input = file.read()
-
     if input == None:
         return
 
     # separate elves by double newline separators
     elves = input.split("\n\n")
 
-    # separate each elf's items by single newline separator and cast as int to send into data structure
+    # separate each elf's items by single newline separator and cast as ints that get summed together
     elfValues = []
     for elf in elves:
-        items = elf.split('\n')
-        itemValues = [int(item) for item in items]
-        elfValues.append(ElfItems(itemValues))
+        elfItems = elf.split('\n')
+        elfValue = sum([int(item) for item in elfItems])
+        elfValues.append(elfValue)
 
-    # get maximum item sum from data structure
-    maxSum = max(elfItems.sum for elfItems in elfValues)
-    print(maxSum)
+    # remove maximum item sum from list
+    topValues = []
+    for i in range(int(maxCount)):
+        highestSum = elfValues.pop(elfValues.index(max(elfValues)))
+        topValues.append(highestSum)
+    print(sum(topValues))
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 1:
         print("Error: input filename not passed in as an argument")
-    elif len(args) > 1:
-        print("Error: too many input arguments passed in")
-    else:
+    elif len(args) == 1:
         main(args[0])
+    elif len(args) == 2:
+        main(args[0], args[1])
+    elif len(args) > 2:
+        print("Error: too many input arguments passed in")
